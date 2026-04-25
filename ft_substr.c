96622d349e73a3250501c7b7ceda396e9d84e2f4
pcastro-42 <pcastro-@student.42porto.com>
2026-04-25 13:10:35 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -3,21 +3,22 @@
 
 char  *ft_substr(char const *s, unsigned int start, size_t len)
 {
-  char  *s2;
-  size_t  i;
+    char  *s2;
+    size_t  i;
 
-  if (start > ft_strlen(s))
-    return (ft_strdup(""));
-  len = ft_strlen(s + start);
-  s2 = malloc((len + 1) * sizeof(char));
-  if (s2 == (void*)0)
-    return ((void*)0);
-  i = 0;
-  while (i < len)
-  {
-    s2[i] = s[i + start];
-    i++;
-  }
-  s2[i] = 0;
-  return (s2);
+    if (start > ft_strlen(s))
+        return (ft_strdup(""));
+    if (len > ft_strlen(s + start))
+        len = ft_strlen(s + start);
+    s2 = malloc((len + 1) * sizeof(char));
+    if (s2 == (void*)0)
+        return ((void*)0);
+    i = 0;
+    while (i < len)
+    {
+        s2[i] = s[i + start];
+        i++;
+    }
+    s2[i] = 0;
+    return (s2);
 }

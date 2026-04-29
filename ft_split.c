b6eb5cec64c67cb83804aa180ddab00ec5d9a9b4
pcastro-42 <pcastro-@student.42porto.com>
2026-04-29 18:57:28 +0100

    Add files via upload

---
+++ ft_split.c
@@ -0,0 +1,81 @@
+#include "libft.h"
+
+static int	count_words(char const *s, char c)
+{
+	int	word_count;
+	int	i;
+	int	is_delimiter;
+
+	word_count = 0;
+	i = 0;
+	is_delimiter = 1;
+	while (s[i])
+	{
+		if (s[i] == c)
+			is_delimiter = 1;
+		else if (is_delimiter)
+		{
+			word_count++;
+			is_delimiter = 0;
+		}
+		i++;
+	}
+	return (word_count);
+}
+
+static size_t	word_len(char *start, char c)
+{
+	int	i;
+
+	i = 0;
+	while (start[i] && start[i] != c)
+	{
+		i++;
+	}
+	return (i);
+}
+
+static char	**split(char **list, char const *s, char c, int wc)
+{
+	int	i;
+	int	j;
+	int	is_delimiter;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	is_delimiter = 1;
+	while (s[i])
+	{
+		if (s[i] == c)
+			is_delimiter = 1;
+		else if (is_delimiter)
+		{
+			is_delimiter = 0;
+			len = word_len(s + i, c);
+			list[j] = malloc(sizeof(char) * (len + 1));
+			if (!list[j])
+				return (NULL);
+			ft_strlcpy(list[j], s + i, len + 1);
+			j++;
+		}
+		i++;
+	}
+	list[j] = 0;
+	return (list);
+
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int	word_count;
+	char	**list;
+
+	if (!s)
+		return (NULL);
+	word_count = count_words(s, c);
+	list = malloc(sizeof(char*) * (word_count + 1));
+	if (!list)
+		return (NULL);
+	return (split(list, s, c, word_count));
+}

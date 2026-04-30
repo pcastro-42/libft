1c780fe4aaeaacbf0b2826c3e46d9a8760df33b2
pcastro-42 <pcastro-@student.42porto.com>
2026-04-30 16:21:52 +0100

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	size_t	i;
+	char	*res;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
